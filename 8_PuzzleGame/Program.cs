830427235836d2aedaecabfb1ea91e60e5371573
Hanselts Medina Salas <hanselts99@gmail.com>
2021-02-26 16:17:56 -0600

    8 Puzzle

---
+++ 8_PuzzleGame/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _8_PuzzleGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] estadoInicialFacil = { 1, 2, 5, 3, 4, 0, 6, 7, 8 } ;
+
+            States initSatate = new States(estadoInicialFacil);
+        }
+
+    }
+}
---
+++ 8_PuzzleGame/States.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _8_PuzzleGame
+{
+    class States
+    {
+        /*public static void Main()
+        {
+            int [] estadoInicialFacil = new int [9] { { 1, 2, 5 }, { 3, 4, 0 }, { 6, 7, 8 } };
+            var estadoInicialMedio = new int[3, 3] { { 3, 2, 5 }, { 8, 0, 6 }, { 1, 4, 7 } };
+            var estadoInicialDificil = new int[3, 3] { { 8, 1, 3 }, { 4, 0, 2 }, { 7, 6, 5 } };
+        }*/
+
+        public List<States> hijos = new List<States>();
+        public States parent;
+        int[] estadoInicialFacil = new int[9];
+        int x = 0;
+        int col = 3;
+
+        public States(int[] puzzle)
+        {
+            setPuzzle(puzzle);
+        }
+
+        public void expandirMoves()
+        {
+            for(int i = 0; i < estadoInicialFacil.Length; i++)
+            {
+                if(estadoInicialFacil[i] == 0)
+                {
+                    x = i;
+                }
+
+                movetoRight(estadoInicialFacil, x);
+                movetoLeft(estadoInicialFacil, x);
+                movetoUp(estadoInicialFacil, x);
+                movetoDown(estadoInicialFacil, x);
+            }
+        }
+        public void movetoRight(int [] p, int i)
+        {
+            if(i % col < col - 1)
+            {
+                int[] puzzle = new int [9];
+                copiaPuzzle(puzzle, p);
+
+                int temp = puzzle[i + 1];
+                puzzle[i + 1] = puzzle[i];
+                puzzle[i] = temp;
+
+                States hijo = new States(puzzle);
+                hijos.Add(hijo);
+                hijo.parent = this;
+            }
+        }
+
+        public void movetoLeft(int[] p, int i)
+        {
+            if (i % col > 0)
+            {
+                int[] puzzle = new int[9];
+                copiaPuzzle(puzzle, p);
+
+                int temp = puzzle[i - 1];
+                puzzle[i - 1] = puzzle[i];
+                puzzle[i] = temp;
+
+                States hijo = new States(puzzle);
+                hijos.Add(hijo);
+                hijo.parent = this;
+            }
+        }
+        public void movetoUp(int[] p, int i)
+        {
+            if (i - col >= 0)
+            {
+                int[] puzzle = new int[9];
+                copiaPuzzle(puzzle, p);
+
+                int temp = puzzle[i - 3];
+                puzzle[i - 3] = puzzle[i];
+                puzzle[i] = temp;
+
+                States hijo = new States(puzzle);
+                hijos.Add(hijo);
+                hijo.parent = this;
+            }
+        }
+        public void movetoDown(int[] p, int i)
+        {
+            if (i + col < estadoInicialFacil.Length)
+            {
+                int[] puzzle = new int[9];
+                copiaPuzzle(puzzle, p);
+
+                int temp = puzzle[i + 3];
+                puzzle[i + 3] = puzzle[i];
+                puzzle[i] = temp;
+
+                States hijo = new States(puzzle);
+                hijos.Add(hijo);
+                hijo.parent = this;
+            }
+        }
+
+        public void imprimirPuzzle()
+        {
+            Console.WriteLine();
+            int meta = 0;
+            for(int i = 0; i < col; i++)
+            {
+                for(int j = 0; j < col; j++)
+                {
+                    Console.WriteLine(estadoInicialFacil[meta] + " ");
+                    meta++;
+                }
+            }
+        }
+
+        public void copiaPuzzle(int[] a, int[] b)
+        {
+            for(int i = 0; i < b.Length; i++)
+            {
+                a[i] = b[i];
+            }
+        }
+
+        public void setPuzzle(int[] puzzle)
+        {
+            for(int i=0; i<estadoInicialFacil.Length; i++)
+            {
+                this.estadoInicialFacil[i] = puzzle[i];
+            }
+        }
+
+        public bool metaAlcanzada()
+        {
+            bool esMeta = false;
+            int meta = estadoInicialFacil[0];
+
+            for(int i=0; i<estadoInicialFacil.Length; i++)
+            {
+                if(meta > estadoInicialFacil[i])
+                {
+                    esMeta = true;
+                }
+                meta = estadoInicialFacil[i];
+            }
+            return esMeta;
+        }
+    }
+}
