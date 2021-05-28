4c605c569ea7652eb74e55f102557d0b69a82222
hanselmed99 <70228326+hanselmed99@users.noreply.github.com>
2021-05-28 17:24:37 -0500

    Add files via upload

---
+++ FirstIA/FirstIA/Grafo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstIA
+{
+    class Grafo
+    {
+        Dictionary<Nodo, List<Nodo>> nodos;
+
+        public Grafo(Dictionary<Nodo, List<Nodo>> nodos)
+        {
+            this.nodos = nodos;
+        }
+
+        public List<Nodo> obtenerNodosAdyacentes(Nodo llave)
+        {
+            return nodos[llave];
+        }
+    }
+}
