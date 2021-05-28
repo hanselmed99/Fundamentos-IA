d309f8d90f89be5a64fb16aee57fba7f812989dc
hanselmed99 <70228326+hanselmed99@users.noreply.github.com>
2021-05-28 15:55:47 -0500

    Add files via upload

---
+++ First IA/ConsoleApp1/Grafo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class Grafo
+    {
+        Dictionary<Nodo, List<Nodo>> nodos;
+        
+        public Grafo(Dictionary<Nodo, List<Nodo>> nodos)
+        {
+            this.nodos = nodos;
+        }
+
+        public List<Nodo> obtenerNodosAdyacentes(Nodo llave)
+        {
+            return nodos[llave];
+        }
+    }
+}
---
+++ First IA/ConsoleApp1/Nodo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class Nodo
+    {
+        public string nombre { get; set; }
+
+        public Nodo(string nombre)
+        {
+            this.nombre = nombre;
+        }
+    }
+}
